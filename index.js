cef2ef80f0f5a084910f8bd41089994505395662
CodingDrumer13 <donadley@gmail.com>
2015-06-25 16:51:40 -0500

    Condensed Code and added comments

--- index.js
+++ index.js
@@ -10,25 +10,23 @@ app.get('/', function(req, res){
 // listen on the connection event for incoming sockets
 io.on('connection', function(socket){
 	console.log('a user connected');
+
+	// Sends a welcome message on the current socket
 	socket.emit('chat message', 'Welcome')
 
+	// Logs that a client on a specific socket has disconnected
 	socket.on('disconnect', function(){
 		console.log('user disconnected');
 	});
 
-	// sends message on socket with tag 'chat message'
-	socket.on('chat message', function(msg){
+	// Logs message
+	// sends message(msg) on socket with tag 'chat message'
+	socket.on('chat message', function(msg){		
 		console.log('message: ' + msg);
-	});
-
-	socket.on('chat message', function(msg){
-  	//send an event to everyone example
 		io.emit('chat message', msg)
-	    // socket.broadcast.emit('chat message', msg); //for everyone except sender
-	  });
+	    // socket.broadcast.emit('chat message', msg); //sends message to everyone except sender
+	});
 });
-
-
 
 http.listen(3000, function(){
 	console.log('listening on *:3000');
